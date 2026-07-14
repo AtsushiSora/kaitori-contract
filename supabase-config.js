543c412beb0fb2cb497bef45c8c373af820127ef
AtsushiSora <closet.full.of.love.a@gmail.com>
2026-07-14 22:12:08 +0900

    Configure Supabase project

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 window.ORDER_AUTO_SUPABASE = {
-  url: "",
-  anonKey: "",
+  url: "https://cumvescylyetumupupmc.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN1bXZlc2N5bHlldHVtdXB1cG1jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQwMjg1NjIsImV4cCI6MjA5OTYwNDU2Mn0.0M-W29Xz11N-Jfwrs5shLOK9htDcz33KwHkP6Z8AKqQ",
   storageBucket: "contract-files",
   publicContractEndpoint: "",
   consentSubmitEndpoint: "",
